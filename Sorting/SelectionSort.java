4a423cb25a2cf5e4094dc528c099106cfed34395
Kanam Ramu <kanamramu18@gmail.com>
2026-04-09 06:05:27 +0530

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,5 +1,5 @@
 public class SelectionSort {
-    public static void SelectionSort(int[] arr) {
+    public static void selectionSort(int[] arr) {
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
